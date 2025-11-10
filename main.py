0bb2f136ffcbeae8c96b82f5780af79809a57216
Agloo055 <jacksonoberman@gmail.com>
2025-11-10 14:43:32 -0800

    add ability to send book paths as arg to main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words, count_characters, sort_chars
+import sys
 
 def get_book_text(filepath):
     file_contents = filepath.read()
@@ -6,20 +7,25 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    with open ("books/frankenstein.txt") as f:
-        text = get_book_text(f)
-        num_words = count_words(text)
-        num_chars = count_characters(text)
-        sorted_letters = sort_chars(num_chars)
+    if len(sys.argv) == 2:    
+        with open (sys.argv[1]) as f:
+            text = get_book_text(f)
+            num_words = count_words(text)
+            num_chars = count_characters(text)
+            sorted_letters = sort_chars(num_chars)
 
-        print("============ BOOKBOT ============")
-        print("Analyzing book found at books/frankenstein.txt...")
-        print("----------- Word Count ----------")
-        print(f"Found {num_words} total words")
-        print("--------- Character Count -------")
-        for letter in sorted_letters:
-            print(f"{letter["char"]}: {letter["num"]}")
-        print("============= END ===============")
+            print("============ BOOKBOT ============")
+            print(f"Analyzing book found at {sys.argv[1]}...")
+            print("----------- Word Count ----------")
+            print(f"Found {num_words} total words")
+            print("--------- Character Count -------")
+            for letter in sorted_letters:
+                print(f"{letter["char"]}: {letter["num"]}")
+            print("============= END ===============")
+
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     
 
 main()
